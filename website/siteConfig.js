e5c4676306e016c48b3640ff206b6c29d9e1a996
andreaortuno <andreaortuno@users.noreply.github.com>
2019-07-25 18:17:47 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'OpenBCI Documentation', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://andreaortuno.github.io', // Your website URL
+  baseUrl: '/Documentation-website-OpenBCI/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'OpenBCI',
+  projectName: 'Documentation-website-OpenBCI',
+  organizationName: 'andreaortuno',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
