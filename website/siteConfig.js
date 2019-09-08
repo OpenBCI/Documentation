e6eb46e4da299a5158a727dd7410360c80db35d9
Richard Waltman <retiutut@icloud.com>
2019-09-07 23:12:34 -0500

    temporarily add baseURL to siteconfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'OpenBCI Documentation', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://openbci.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/Documentation/', // Base URL for your project */
   //cname: 'docs.openbci.com',
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
