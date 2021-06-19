0a99600583f5c62f86105afe22f0e054e5ec480f
Richard Waltman <retiutut@icloud.com>
2021-06-18 20:50:54 -0500

    docusaurus v2 - CLEANUP IMAGE LINKS - WIP half fixed

--- websitev2/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/Richard/Documents/GitHub/Documentation/websitev2/src/css/customTheme.css"),
-];
