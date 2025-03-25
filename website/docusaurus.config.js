f12df97787ffd604d5881de15ea7026e7455228d
Richard Waltman <richard@openbci.com>
2025-03-25 11:49:46 -0500

    Update algolia settings in Docusaurus config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -96,11 +96,36 @@ module.exports = {
       },
     },
     algolia: {
-      appId: "BH4D9OD16A",
-      apiKey: "19411ba246745c95db0bff87cfed97b0",
+      // The application ID provided by Algolia
+      appId: "T4RYDUBT8W",
+
+      // Public API key: it is safe to commit it
+      apiKey: "a76eb38acd08673522c818bdb7b97feb",
+
       indexName: "openbci",
-      placeholder: "Search Docs",
-      algoliaOptions: {},
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      //externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
     },
   },
 }
